9c8db09f3f3daad900c08c5ca9a0a01fbf0f7b43
AlejandroMV03 <alejandromexxx894@gmail.com>
2025-05-16 15:10:42 -0600

    Proyecto React con Tailwind y NavBar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <div className="text-center mt-10 text-3xl text-green-600 font-bold">
+        ¡SEAN BIENVENIDOS A MI ENTORNO DE TRABAJO, SOY CRISTIAN ALEJANDRO MEX VILLACIS!
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <nav className="bg-blue-600 text-white p-4">
+      <h1 className="text-2xl font-semibold">Entorno de Trabajo</h1>
+    </nav>
+  );
+}
